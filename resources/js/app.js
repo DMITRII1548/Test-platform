b20c32ea18e2ba39ff53c8f13f3400c05d4fce34
DMITRII1548 <korsunov2006@list.ru>
2024-01-19 21:42:09 +0600

    Vue.js installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp, VueElement } from 'vue/dist/vue.esm-bundler'
+import AppComponent from './App.vue'
+
+
+const app = createApp({
+    components: {
+        AppComponent,
+    }
+})
+
+app.mount('#app')
