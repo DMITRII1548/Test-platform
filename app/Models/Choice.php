e188830bcab3b35bf88016a79489ccad94cd09e2
DMITRII1548 <korsunov2006@list.ru>
2024-01-19 19:34:38 +0600

    Added fillable for models

--- app/Models/Choice.php
+++ app/Models/Choice.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Choice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'is_true',
+        'title',
+        'question_id'
+    ];
 }
